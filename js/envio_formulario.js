eda1d2e6f0c68c0ccb3e88b737438b259ad00f9d
MartinMedinaRuvian <martindjmedina@hotmail.com>
2021-05-08 12:35:28 -0500

    directorio font

--- js/envio_formulario.js
+++ js/envio_formulario.js
@@ -1,14 +1,10 @@
-const form = document.querySelector('.form');
-
-form.addEventListener('submit', sendMessage);
+document.querySelector('.form').addEventListener('submit', sendMessage);
 
 function sendMessage(event) {
     event.preventDefault();
 
-
     const formData = new FormData(this);
     const btnsend = document.querySelector('#btnsend');
-    
 
     const messageFormat = `mailto:martindjmedina@hotmail.com, martinruvian@gmail.com?cc=${formData.get('email')}&subject=Desde: martindjmedina.com. Nombre: ${formData.get('nombre')}&body=Mensaje: ${formData.get('mensaje')} Contacto: ${formData.get('email')}`;
 
